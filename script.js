be3fb227a5be78625870e8c6709f63143ba1c5ce
valentinciolofan <valentincatalin02@gmail.com>
2024-05-27 14:43:47 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const accordion = document.querySelectorAll(".accordion-faq");
-
+const hamburgerBtn = document.querySelector('#hamburger');
+const navbar = document.querySelector(".navbar");
+console.log(hamburgerBtn);
 
 
 accordion.forEach(accordion => {
@@ -8,3 +10,7 @@ accordion.forEach(accordion => {
     faqSlideDown.classList.toggle("is-open");
   });
 });
+
+hamburgerBtn.addEventListener("click", () => {
+  navbar.classList.toggle("is-open");
+})
